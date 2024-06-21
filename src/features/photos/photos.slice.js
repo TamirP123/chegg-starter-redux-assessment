e7787278ba045ee2b41f88984280c740e9c4086e
Tamir <tamirphillips@live.com>
2024-06-20 20:38:40 -0400

    Added addPhoto reducer

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -13,7 +13,9 @@ const options = {
     // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
     // Task 1 Hint: You can use state.photos.unshift()
     // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-   
+    addPhoto: (state, action) => {
+      state.photos.push({id : state.photos.length + 1, ... action.payload});
+    },
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
     // Task 6 Hint: You can use state.photos.splice()
     // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
